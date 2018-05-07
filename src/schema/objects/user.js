b674ab8bc463e6301e0bf9b024692a4756604c85
Eugene Kovalev <seanchas@gmail.com>
2018-05-07 19:35:58 +0300

    User's loan list

--- src/schema/objects/user.js
+++ src/schema/objects/user.js
@@ -1,3 +1,4 @@
+import { chainFetch } from "/transport"
 import { globalIdField, createDefinition } from "/schema/utils"
 
 
@@ -5,10 +6,20 @@ const definition = `
   type User implements Node {
     id: ID!
     email: String!
+    loans: [Loan!]!
     shouldProvidePassword: Boolean!
     shouldLoginWithOTPAuth: Boolean
   }
 `
+
+
+const loans = async user => {
+  const response = await chainFetch({
+    user_id: "1.2.144",
+    oper: "get_credit_request_ids",
+  })
+  return response.credit_request_ids.map(id => ({ id }))
+}
 
 
 const shouldProvidePassword = user => {
@@ -21,6 +32,7 @@ export default createDefinition(
   {
     User: {
       id: globalIdField(),
+      loans,
       shouldProvidePassword,
     }
   }
