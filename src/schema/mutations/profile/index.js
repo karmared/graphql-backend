1e04f4e01352b1eded6ff0ff3c753cd6217537d9
Eugene Kovalev <seanchas@gmail.com>
2018-07-02 10:47:14 +0300

    Remove profile

--- src/schema/mutations/profile/index.js
+++ src/schema/mutations/profile/index.js
@@ -1,9 +1,11 @@
+import removeProfile from "./removeProfile"
 import createIndividualProfile from "./createIndividualProfile"
 import updateIndividualProfile from "./updateIndividualProfile"
 import { collectDefinitions } from "/schema/utils"
 
 
 export default collectDefinitions(
+  removeProfile,
   createIndividualProfile,
   updateIndividualProfile,
 )
---
+++ src/schema/mutations/profile/removeProfile.js
@@ -0,0 +1,77 @@
+import is from "is_js"
+import store from "/store"
+import schema from "/schema"
+import { ValidationError } from "/errors"
+import { fromGlobalId, createDefinition } from "/schema/utils"
+
+
+const definition = /* GraphQL */`
+  input RemoveProfileInput {
+    id: ID!
+  }
+
+  type RemoveProfilePayload {
+    user: User!
+  }
+
+  extend type Mutation {
+    removeProfile(input: RemoveProfileInput!): RemoveProfilePayload!
+  }
+`
+
+
+const findProfileForUser = async (userId, profileId) => {
+  const connection = await store.connect()
+
+  return store.table("user_profiles")
+    .filter(profile => profile("user")("id").eq(userId))
+    .filter(profile => profile("id").eq(profileId))
+    .nth(0)
+    .run(connection)
+    .then(record => {
+      connection.close()
+      return record
+    })
+    .catch((error) => {
+      connection.close()
+      return null
+    })
+}
+
+
+const removeProfile = async (root, { input }, { viewer }) => {
+  if (is.not.existy(viewer))
+    throw new ValidationError({
+      keyword: "presence",
+      dataPath: "/viewer",
+    })
+
+  const profile = await findProfileForUser(
+    viewer.id,
+    fromGlobalId(input.id).id,
+  )
+  if (is.not.existy(profile))
+    throw new ValidationError({
+      keyword: "presence",
+      dataPath: "/profile",
+    })
+
+  await store.node.remove(
+    schema.getType("UserProfile").resolveType(profile),
+    profile.id,
+  )
+
+  return {
+    user: schema.getType("User").fetch(viewer.id)
+  }
+}
+
+
+export default createDefinition(
+  definition,
+  {
+    Mutation: {
+      removeProfile,
+    }
+  }
+)
